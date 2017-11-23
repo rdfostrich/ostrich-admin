ac9be57a4d72715a5dbad71511dccbd72277101b
Ruben Taelman <ruben.taelman@ugent.be>
2017-11-23 16:36:14 +0900

    Add VM query interface

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,40 @@ if (!path) {
   throw new Error('No OSTRICH path was provided.');
 }
 
+var lastTotalCount = -1;
+
 prepare(path, function (store) {
   router.get('/', function(req, res, next) {
     getStats(path, store).then(function (stats) {
-      res.render('index', Object.assign({ title: 'OSTRICH Demo'}, stats ));
+      res.render('index', Object.assign({ title: 'OSTRICH Demo'}, stats));
+    });
+  });
+
+  router.get('/qvm', function(req, res, next) {
+    getStats(path, store).then(function (stats) {
+      var query = Object.assign({
+        subject: '',
+        predicate: '',
+        object: '',
+        offset: 0,
+        limit: 20,
+        version: store.maxVersion,
+      }, req.query);
+
+      var startTime = getTimeMs();
+      store.searchTriplesVersionMaterialized(query.subject, query.predicate, query.object,
+        { version: query.version, offset: query.offset, limit: query.limit }, function (error, triples, count, exact) {
+          var endTime = getTimeMs();
+          res.render('qvm', Object.assign({ title: 'Version Materialization'}, stats,
+            {
+              query: query,
+              triples: triples,
+              currentCount: triples.length,
+              count: count,
+              countType: exact ? "Exact" : "Estimate",
+              duration: (endTime - startTime).toFixed(3)
+            }));
+      });
     });
   });
 
@@ -37,18 +67,41 @@ function prepare(path, cb) {
 
 function getStats(path, store) {
   return new Promise(function (resolve, reject) {
-    getFolderSize(path, function (err, size) {
-      if (err) {
-        reject(err);
-      }
-      var stats = {
-        path: path,
-        versions: store.maxVersion,
-        size: (size / 1024 / 1024).toFixed(2),
-      };
+    getTotalCount(store).then(function(triples) {
+      getFolderSize(path, function (err, size) {
+        if (err) {
+          reject(err);
+        }
+        var stats = {
+          path: path,
+          versions: store.maxVersion,
+          size: (size / 1024 / 1024).toFixed(2),
+          totalTriples: triples,
+        };
 
-      resolve(stats);
-    });
+        resolve(stats);
+      });
+    }).catch(reject);
   });
+}
+
+function getTotalCount(store) {
+  return new Promise(function (resolve, reject) {
+    if (lastTotalCount > -1) {
+      resolve(lastTotalCount);
+    } else {
+      store.countTriplesVersion(null, null, null, function (error, triples) {
+        if (error) {
+          reject(error);
+        }
+        resolve(lastTotalCount = triples);
+      });
+    }
+  });
+}
+
+function getTimeMs() {
+  var hrTime = process.hrtime();
+  return hrTime[0] * 1000 + hrTime[1] / 1000000;
 }
 
